79bba5a455aeea1589b12af146cff803aa55f1e2
pearcore <pearcore@sohu.com>
2020-01-03 10:58:15 +0800

    zzz

--- psite/settings.py
+++ psite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pwpusq*647(-gi4x$iehimwrhm-92!_)thwe4ok$4k!c$+&kn%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
