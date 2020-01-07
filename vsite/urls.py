6717a44a685e10b56a92bc16160fed4646770401
pearcore <pearcore@sohu.com>
2020-01-07 16:04:00 +0800

    zz

--- vsite/urls.py
+++ vsite/urls.py
@@ -4,10 +4,6 @@ from django.urls import path,include
 from django.urls import include, path #
 from rest_framework import routers #
 
-#router = routers.DefaultRouter() #
-#router.register(r'users', views.UserViewSet) #
-#router.register(r'groups', views.GroupViewSet) #
-
 urlpatterns = [
     path('netask/', include('netask.urls')),
     path('admin/', admin.site.urls),
