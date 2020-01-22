cec33732a01382b7377a3f4e5d7a503481965f8a
pearcore <pearcore@sohu.com>
2020-01-22 11:03:47 +0800

    zz

--- psite/settings.py
+++ psite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2%pjeixzu2q+bl8x7_l*$&lcx-2ydat9ns=x=&p9z)-_9tsidt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
