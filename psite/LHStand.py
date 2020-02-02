ef05636a111099a96f73dabfaa74fb02eca9ac95
pearcore <pearcore@sohu.com>
2020-02-02 16:31:07 +0800

    '2'

--- psite/LHStand.py
+++ psite/LHStand.py
@@ -19,7 +19,7 @@ class LHKit:
     def LHResult():
         rtModel = { # return model
         "code":1000,
-        "msg":"ok2!",
+        "msg":"ok~",
         "data":0,
         }
         return rtModel
