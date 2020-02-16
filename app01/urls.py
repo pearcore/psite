d98a0a8390b96212b4d9bb55e8739d9c92748080
pearcore <pearcore@sohu.com>
2020-02-16 13:33:29 +0800

    zz

--- app01/urls.py
+++ app01/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^(?P<version>[v1|v2]+)/roles/$' , views.RolesView.as_view()),
     url(r'^(?P<version>[v1|v2]+)/userinfos/$' , views.UserInfosView.as_view()),
     #url(r'^(?P<version>[v1|v2]+)/group/(?P<pk>\d+)$' , views.GroupView.as_view()),
-    url(r'^(?P<version>[v1|v2]+)/group/$' , views.GroupView.as_view())
+    url(r'^(?P<version>[v1|v2]+)/group/(?P<pk>\d+)$' , views.GroupView.as_view(),name='gp')
 
 
 ]
