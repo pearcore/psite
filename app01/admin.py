d15d120432510c7aa664b9707588a55575533d2b
pearcore <pearcore@sohu.com>
2020-06-19 19:56:35 +0800

    '2'\

--- app01/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserInfo,UserToken,UserRole,UserGroup
-
-admin.site.register(UserInfo)
-admin.site.register(UserToken)
-
-admin.site.register(UserRole)
-admin.site.register(UserGroup)
--- app01/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class App01Config(AppConfig):
-    name = 'app01'
-
-    verbose_name = 'app01系统'
