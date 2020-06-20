8ed05c5d448d3da22d85fa978f30090c95725101
pearcore <pearcore@sohu.com>
2020-06-20 16:57:43 +0800

    '新项目第一次提交

---
+++ app01/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,HttpResponse
+import json
+from psite.LHStand import LHKit
+# Create your views here.
+def users(request):
+    user_list = ["Test","dkdkdk"]
+    rt = LHKit.LHResult()
+    rt['data'] = user_list
+    return HttpResponse( json.dumps( rt ) )
+
+from django.views import View
+
+class StudentsView(View):
+    def get(self,request , *args, **kwargs):
+        user_list = ["dddd","aaaaa"]
+        rt = LHKit.LHResult()
+        rt['data'] = user_list
+        return HttpResponse( json.dumps( rt ) )
+    def post(self,request , *args, **kwargs):
+        user_list = ["Post","Post"]
+        rt = LHKit.LHResult()
+        rt['data'] = user_list
+        return HttpResponse( json.dumps( rt ) )
