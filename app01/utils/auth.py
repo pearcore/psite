54ebe1612d5c29985ce69850a5470eb967df1742
pearcore <pearcore@sohu.com>
2020-07-04 15:34:13 +0800

    auth

--- app01/utils/auth.py
+++ app01/utils/auth.py
@@ -1,15 +1,16 @@
 from django.http import JsonResponse
 from app01 import models
 from rest_framework import exceptions
+from rest_framework.authentication import BaseAuthentication
 
-class FirstAuthtication(object):
+class FirstAuthtication(BaseAuthentication):
     def authenticate(self, request):
         pass
     def authenticate_header(self,val):
         pass
 
 
-class Authtication(object):
+class Authtication(BaseAuthentication):
     def authenticate(self, request):
         token = request._request.GET.get('token')
         token_obj = models.UserToken.objects.filter(token=token).first()
@@ -17,5 +18,11 @@ class Authtication(object):
             raise exceptions.AuthenticationFailed('用户认证失败')
         #在restframework内部会将两个字段赋值给request，以供后面使用。
         return (token_obj.user,token_obj)
+
+        #return None (下一个认证执行，我不管了。)
+        #raise exceptions.AuthenticationFailed('用户认证失败') 认证失败
+        #return (token_obj.user,token_obj) 认证通过，返回认证的用户信息和token信息。
+
+
     def authenticate_header(self,val):
         pass
