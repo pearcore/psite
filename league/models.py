cda04d8258bf70144d28bdefee3e0cca2cb8d3e5
pearcore <pearcore@sohu.com>
2020-12-04 20:11:31 +0800

    'zz'

--- league/models.py
+++ league/models.py
@@ -2,13 +2,18 @@ from django.db import models
 # Create your models here.
 class PlayerLogin(models.Model):
     class Meta:
-        verbose_name = 'Player'
-        verbose_name_plural = 'Players' 
+        verbose_name = 'PlayerLogin'
+        verbose_name_plural = 'PlayerLogin' 
     mobile = models.CharField(max_length = 32 , unique=True)
     password = models.CharField(max_length = 64)
     def __str__(self):
         return self.mobile
 
 class PlayerToken(models.Model):
+    class Meta:
+        verbose_name = 'PlayerToken'
+        verbose_name_plural = 'PlayerToken' 
     user = models.OneToOneField(to='PlayerLogin', on_delete = models.CASCADE)
     token = models.CharField(max_length=64)
+    def __str__(self):
+        return self.user.mobile
